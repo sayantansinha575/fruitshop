38b1388cfaf71c7aa3018f7c405703269e0d0faf
sayantansinha575 <sayantansinha575@gmail.com>
2024-08-10 14:26:07 +0530

    add static path

--- process.php
+++ process.php
@@ -3,10 +3,12 @@ if (isset($_GET['id'])) {
     $fileId = $_GET['id'];
 
     // Define the path to the file or use a lookup to get the file
-    $filePath = 'https://0fec905c-f435-4138-8774-61e7f299ad41.e1-us-cdp-2.choreoapps.dev/file/' . $fileId . '.txt';
-
+    // $filePath = './file/' . $fileId . '.txt';
+    if ($fileId == 123) {
+        $filePath = 'https://img.freepik.com/premium-photo/natures-beauty-captured-colorful-flower-close-up-generative-ai_1038396-47.jpg';
+    }
     // Ensure the file exists
-    if (file_exists($filePath)) {
+    if (($filePath)) {
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
         header('Content-Disposition: attachment; filename="' . basename($filePath) . '"');
